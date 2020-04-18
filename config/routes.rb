7f32d75fec780f29b8939acb67d89ff85e1c5499
Ann-Marie Harper <annmarie.grace.harper@gmail.com>
2020-04-18 12:36:46 -0500

    Added CSS for Home Page, Buttons, and About Us page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'home', to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts
+  get 'about', to: 'about#about'
 end
