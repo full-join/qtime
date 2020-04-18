732e6684683f87c2e208e32d3eddfe0a8597c3d1
Alison Pepper <alison.l.dore@gmail.com>
2020-04-18 12:37:40 -0500

    New Login Logic

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+  def new
+  end
+  def create
+    @user = User.create!(user_params)
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
+  end
+  private
+  def user_params
+    params.require(:user).permit(:username, :name, :email, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get 'home', to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts
+  get 'login', to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
