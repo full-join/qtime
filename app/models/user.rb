de00b537afd5986e4afa72bcd675b70060cbeb15
Alison Pepper <alison.l.dore@gmail.com>
2020-04-23 13:38:07 -0500

    Login fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,4 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id              :bigint           not null, primary key
-#  email           :string
-#  name            :string
-#  username     :string
-#  created_at      :datetime         not null
-#  updated_at      :datetime         not null
-#  password_digest :string
-#
+
 
 class User < ApplicationRecord
   has_secure_password
