b939cf28c21b19d0dd38449195094c9cfc8ea96d
Ann-Marie Harper <annmarie.grace.harper@gmail.com>
2020-05-07 13:59:41 -0500

    Added connection to Google Places API

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,16 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function () {
+  var searchInput = 'searchbar';
+  var autocomplete;
+  autocomplete = new google.maps.places.Autocomplete((document.getElementById(searchInput)), {
+    type: ['grocery_or_supermarket'],
+    fields: ["name", "address_component", "formatted_address", "geometry.location", "opening_hours"]
+    });
+
+google.maps.event.addListener(autocomplete, 'place_changed', function () {
+  var near_place = autocomplete.getPlace();
+
+  });
+
+});
