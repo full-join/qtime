0dacdafa3990cea3469c3cea5a5ffbbe99d128e5
Ann-Marie Harper <annmarie.grace.harper@gmail.com>
2020-05-15 12:33:31 -0500

    Updated navbar header from 'Stores' to 'Additional Resources'
    changed 'stores' in header navbar to 'Additional Resources'; added resources#index route, resources controller, resources_index erb with links to other helpful websites

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'profile', to: 'users#show'
   resources :users do
   end
+  resources :resources, only: [:index]
   get 'login', to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
