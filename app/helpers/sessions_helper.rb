5bf548e1c36d597c30ea0b45d5c5f6bc0ae999c0
Julie Nisbet <jmnisbet@gmail.com>
2020-05-29 10:18:13 -0700

    [NO TICKET] fix rubocop issues
    updates all existing files so we have a clean rubocop build moving forward

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,12 +1,10 @@
+# frozen_string_literal: true
+
 module SessionsHelper
-  def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
-    end
-  end
   def logged_in?
     !session[:user_id].nil?
   end
+
   def logout_current_user
     session[:user_id] = nil
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  root 'home#index'
-  get 'home', to: 'home#index'
+  root "home#index"
+  get "home", to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts
-  get 'profile', to: 'users#show'
+  get "profile", to: "users#show"
   resources :users do
     resources :likes
   end
-  get 'login', to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  get "login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   resources :sessions
-  get 'about', to: 'about#about'
+  get "about", to: "about#about"
 end
